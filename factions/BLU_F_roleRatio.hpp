b47087f798ed0e6adabbc2c919f5d968d07d3f22
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:42:01 +0200

    tb_rhs_la_biblioteca.altis mission added

---
+++ factions/BLU_F_roleRatio.hpp
@@ -0,0 +1,20 @@
+// PMC - GK Sierra
+// Author: Cole, Alonso
+
+{
+	// {id, "caption", teamRatio, minimum, minTeamSize, minSquadSize, squadRatio}
+	{__ICE_gear_Rifleman, "Rifleman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Grenadier, "Grenadier", 5, 0, 0, 3, 1},
+	{__ICE_gear_MG, "MG", 5, 0, 0, 3, 1},
+	{__ICE_gear_Medic, "Medic", 3, 1, 0, 3, 0},
+	{__ICE_gear_CombatEngineer, "Combat Engineer", 0, 0, 0, 0, 0},
+	{__ICE_gear_LightAT, "Light AT", 0, 0, 0, 0, 0},
+	{__ICE_gear_HeavyAT, "Medium AT", 0, 0, 0, 0, 0},
+	{__ICE_gear_AA, "Anti-Air", 0, 0, 0, 0, 0},
+	{__ICE_gear_Marksman, "Marksman", 10, 0, 0, 3, 0},
+	{__ICE_gear_Sniper, "Sniper", 0, 0, 0, 0, 0},
+	{__ICE_gear_SL, "Squad Leader", __unlimited, 0, 0, 1, 0},
+	{__ICE_gear_Crewman, "Crewman", 0, 0, 0, 0, 0},
+	{__ICE_gear_Pilot, "Pilot", 10, 0, 0, 1, 0},
+	{__ICE_gear_Diver, "UAV Operator", 0, 0, 0, 0, 0}
+},
